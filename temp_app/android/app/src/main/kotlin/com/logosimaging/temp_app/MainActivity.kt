f251d4bd99f36179031cfb5b96ac13e494754736
Michael Martin <mmartin@logosimaging.com>
2024-05-02 09:19:35 -0600

    Minimum app and plugin

---
+++ temp_app/android/app/src/main/kotlin/com/logosimaging/temp_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.logosimaging.temp_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ temp_package/lib/temp_package.dart
@@ -0,0 +1,7 @@
+library temp_package;
+
+/// A Calculator.
+class ImageProcessor {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
